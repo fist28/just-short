4dc40841741d01c8f506cc0825e59421ba785995
Filip Stybel <filip.stybel@justcode.io>
2018-10-10 16:37:06 +0200

    Add redirect to simple 404 page if mongo not found document

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  rescue_from Mongoid::Errors::DocumentNotFound do |exception|
+    render plain: '404 Not found', status: :not_found
+  end
 end
