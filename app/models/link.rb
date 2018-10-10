ba00a9748a5df1c2b40354cc96342e5e3d6f369e
Filip Stybel <filip.stybel@justcode.io>
2018-10-10 15:02:03 +0200

    Add Link model

---
+++ app/models/link.rb
@@ -0,0 +1,20 @@
+class Link
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :destination, type: String
+  field :_id, type: String, default: -> { Link.generate_id }
+
+  private
+
+  def self.generate_id
+    loop do
+      id = SecureRandom.hex(4)
+      break id unless id_present?(id)
+    end
+  end
+
+  def self.id_present?(id)
+    Link.where(_id: id).present?
+  end
+end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -2,8 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Link do
   let(:link) { Link.create(destination: 'https://example.com') }
-  xit 'create link object' do
-    expect(link).to eq Link.last
+
+  it 'link has created_at field' do
+    expect(link.created_at).to be
+  end
+  it 'link has updated_at field' do
+    expect(link.updated_at).to be
   end
 
   context 'generate id' do
@@ -16,6 +20,15 @@ RSpec.describe Link do
       ids = []
       100.times { ids << Link.generate_id }
       expect(ids.uniq.size).to eq 100
+    end
+  end
+
+  context '#id_present(id)' do
+    it 'return true if object exist in database' do
+      expect(Link.id_present?(link.id)).to be_truthy
+    end
+    it 'return false if object not exist in database' do
+      expect(Link.id_present?('12345678')).to be_falsey
     end
   end
 end
