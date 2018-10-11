e8b33f67e8142a4baa958cb7ff8209e1ae10401b
Filip Stybel <filip.stybel@justcode.io>
2018-10-11 08:44:15 +0200

    Add redirect to root path for not mathed paths

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'links#new'
   get '/:id', to: 'links#redirect', constraints: { id: /[a-zA-Z\d]{8}/ }
   resources :links, only: %i(new show create)
+  get '*path', to: redirect('/')
 end
