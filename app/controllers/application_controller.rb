e8b22131995c08cecc493fa0c78ab129c7114720
Filip Stybel <filip.stybel@justcode.io>
2018-10-11 08:42:34 +0200

    Set locale for app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
   rescue_from Mongoid::Errors::DocumentNotFound do |exception|
     render file: "#{Rails.root}/public/404", status: :not_found
+  end
+
+  def set_locale
+    I18n.locale = extract_locale_from_accept_language_header
+  end
+
+  private
+  def extract_locale_from_accept_language_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
   end
 end
